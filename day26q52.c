9863a081b9e7bd623d08671a66d902c038acae9f
bhumika-1906 <vbhumika783@gmail.com>
2026-09-04 21:35:42 +0530

    Add files via upload

---
+++ day26q52.c
@@ -0,0 +1,42 @@
+/*
+Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+*/
+
+#include <stdio.h>
+
+int main() {
+    int pattern[] = {1, 3, 5, 3, 1};
+    int i, j;
+    
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < pattern[i]; j++) {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    return 0;
+}
