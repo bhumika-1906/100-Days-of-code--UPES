eb8b1c37172390d987d2805bc05d94cce5edb9db
bhumika-1906 <vbhumika783@gmail.com>
2026-09-10 21:30:15 +0530

    Add files via upload

---
+++ day32q63.c
@@ -0,0 +1,38 @@
+/*
+Question:
+Q63: Merge two arrays.
+Sample Test Cases:
+Input 1:
+3
+1 2 3
+2
+4 5
+Output 1:
+1 2 3 4 5
+*/
+#include <stdio.h>
+int main() {
+int n1, n2;
+if (scanf("%d", &n1) != 1 || n1 <= 0) return 0;
+int arr1[n1];
+for (int i = 0; i < n1; i++) {
+scanf("%d", &arr1[i]);
+}
+if (scanf("%d", &n2) != 1 || n2 <= 0) return 0;
+int arr2[n2];
+for (int i = 0; i < n2; i++) {
+scanf("%d", &arr2[i]);
+}
+int merged[n1 + n2];
+for (int i = 0; i < n1; i++) {
+merged[i] = arr1[i];
+}
+for (int i = 0; i < n2; i++) {
+merged[n1 + i] = arr2[i];
+}
+for (int i = 0; i < n1 + n2; i++) {
+printf("%d ", merged[i]);
+}
+printf("\n");
+return 0;
+}
